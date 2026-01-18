60e9840b7252554045f74ed76b6576475d62a1b4
aadityasaini2004 <aditysaini472894@gmail.com>
2026-01-18 14:35:48 +0530

    Backend Setup With Java + Spring Boot

---
+++ src/test/java/com/college/helpDesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.helpDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
